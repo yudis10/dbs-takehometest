78a4dd997e4db315e84b88db06e54ea592117e7d
Yudis <yudistira@detik.com>
2023-02-05 17:16:01 +0700

    setup github pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  createHashRouter,
+} from "react-router-dom";
 
 import MainLayout from "./layouts/MainLayout";
 import Home from "./pages/Home";
 import AddUser from "./pages/AddUser";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <MainLayout />,
+      children: [
+        { index: true, element: <Home /> },
+        { path: "/form", element: <AddUser /> },
+      ],
+    },
+    { path: "/nolayout", element: <Home /> },
+  ],
   {
-    path: "/",
-    element: <MainLayout />,
-    children: [
-      { index: true, element: <Home /> },
-      { path: "/form", element: <AddUser /> },
-    ],
-  },
-  { path: "/nolayout", element: <Home /> },
-]);
+    basename: "/dbs-takehometest/",
+  }
+);
 
 function App() {
   return <RouterProvider router={router} />;
